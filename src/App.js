a6c4ec0c2986def297a804884e8db0b1b953a391
Vinicius Dornelles de Sousa <vinicius_sousa@hst.com.br>
2020-07-15 16:28:13 -0300

    HomePage files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import HomePage from './homepage.component';
+
 function App() {
-  return <div className="App" />;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
 
 export default App;
